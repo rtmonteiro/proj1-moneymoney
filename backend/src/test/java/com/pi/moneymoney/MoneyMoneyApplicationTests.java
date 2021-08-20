0b7c3d98ac803488cdeb1df20b71b6dafa28dec6
Rogério Medeiros <52194210+RogerioMSantos@users.noreply.github.com>
2021-08-20 19:49:12 -0300

    Adiciona arquivos de configuração spring

---
+++ backend/src/test/java/com/pi/moneymoney/MoneyMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.moneymoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
